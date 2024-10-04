6046efaa6f8a084c615afc5030a99d86ff6e9f4b
Surabhishahapure3 <surabhishahapure3@gmail.com>
2024-10-04 12:17:40 +0530

    modified header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
@@ -10,11 +10,13 @@ import { DataService } from 'src/services/data-service/data.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  @Output() toggleDrawer = new EventEmitter<void>();
   searchQuery:string = ""
   showProfile: boolean = false;
-  userName: string = localStorage.getItem('fstname') || ''
-  userEmail: string = localStorage.getItem('eml') || '';
+  userName: string = '';
+  userEmail: string = '';
+  // console.log(userName,userEmail);
   constructor( iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private dataService: DataService, private router: Router) {
     iconRegistry.addSvgIconLiteral('main-menu', sanitizer.bypassSecurityTrustHtml(MENU_ICON));
     iconRegistry.addSvgIconLiteral('search-icon', sanitizer.bypassSecurityTrustHtml(SEARCH_ICON));
@@ -25,7 +27,17 @@ export class HeaderComponent {
 
   ngOnInit():void{
 
+    this.loadUserData();
   }
+
+  loadUserData() {
+    this.userName = localStorage.getItem('name') || '';  // Ensure correct key 'name'
+    this.userEmail = localStorage.getItem('email') || '';  // Ensure correct key 'email'
+    // console.log('User Name:', this.userName);
+    // console.log('User Email:', this.userEmail);
+  }
+
+
   logout(){
     localStorage.clear();
     this.router.navigate(['/'])
@@ -34,9 +46,9 @@ export class HeaderComponent {
     this.dataService.updateSearchQuery(this.searchQuery)
   }
 
-  toggleProfileDropdown() {
-    this.showProfile = !this.showProfile;
-}
+  onMenuIconClick(){
+    this.toggleDrawer.emit();
+  }
 
 
 }
